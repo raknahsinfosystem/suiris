9f2702d2daada747f59e523c205fbbc08cf530ea
Shankar <Shankar@ShankarRK>
2018-05-13 13:29:33 +0530

    FIRST COMMIT

---
+++ src/main/java/com/raknahs/suiris/suiris/SuirisApplication.java
@@ -0,0 +1,12 @@
+package com.raknahs.suiris.suiris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuirisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuirisApplication.class, args);
+	}
+}
---
+++ src/test/java/com/raknahs/suiris/suiris/SuirisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.raknahs.suiris.suiris;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SuirisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
